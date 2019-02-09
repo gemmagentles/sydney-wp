28b8646fa9d57a2bb761a8ab81562dd329383232
gemmagentles <multimediadesign.gemmagentles@gmail.com>
2019-02-08 20:33:34 -0800

    create a new component to hold an optional heading and wysiwyg paragraph.

---
+++ wp-content/themes/sydneytheme/partials/centered-text.php
@@ -0,0 +1,7 @@
+<div class="centered-text__wrapper">
+    <div class="centered-text__container">
+        <h1 class="centered-text__heading"><?php the_field( 'centered_text_heading' ); ?></h1>
+        <!-- wysiwyg -->
+        <?php the_field( 'centered_text_aparagraph' ); ?>
+    </div>
+</div>
