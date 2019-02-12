0c2455dd49ba28a103fa3fe9ff9c7f1e6c0bb80f
gemmagentles <multimediadesign.gemmagentles@gmail.com>
2019-02-11 20:37:32 -0800

    change the style of the centered text component to consider if the client adds a paragraph not just a heading.

--- wp-content/themes/sydneytheme/partials/centered-text.php
+++ wp-content/themes/sydneytheme/partials/centered-text.php
@@ -1,7 +1,9 @@
 <div class="centered-text__wrapper">
     <div class="centered-text__container">
         <h1 class="centered-text__heading"><?php the_field( 'centered_text_heading' ); ?></h1>
-        <!-- wysiwyg -->
-        <?php the_field( 'centered_text_paragraph' ); ?>
+        <?php if ( the_field( 'centered_text_paragraph' ) ) : ?>
+            <!-- wysiwyg -->
+            <div class="centered-text__paragraph"><?php the_field( 'centered_text_paragraph' ); ?></div>
+        <?php endif; ?>
     </div>
 </div>
