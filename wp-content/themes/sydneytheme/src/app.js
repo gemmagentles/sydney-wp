59c447ffcddac3ad3d31b5bab8011f65a3cce50c
gemmagentles <multimediadesign.gemmagentles@gmail.com>
2019-02-25 17:03:54 -0800

    add a global style to remove the blue focus outline border. As now the accordion is there it should not have a border when clicked with mouse as it stays there when the accordion is open. But for accessbility check if the user is tabbing with the keyboard instead of mouse, and if so add the outline focus.

--- wp-content/themes/sydneytheme/src/app.js
+++ wp-content/themes/sydneytheme/src/app.js
@@ -3,3 +3,24 @@ import './sass/main.scss';
 import './components/header.js';
 import './components/slider.js';
 import './components/pdf-block.js';
+
+
+(function ($, root, undefined) {
+
+    $(function () {
+
+        'use strict';
+
+        // Listen to tab events to enable outlines (accessibility improvement)
+        function handleFirstTab(e) {
+            if (e.keyCode === 9) { // the "I am a keyboard user" key
+                document.body.classList.add('user-is-tabbing');
+                window.removeEventListener('keydown', handleFirstTab);
+            }
+        }
+        
+        window.addEventListener('keydown', handleFirstTab);
+
+    });
+
+})(jQuery, this);
