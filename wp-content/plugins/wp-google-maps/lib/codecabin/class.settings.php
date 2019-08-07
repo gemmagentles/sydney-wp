959e1cd783363488944055d31009aa2474c28dc2
gemmagentles <multimediadesign.gemmagentles@gmail.com>
2019-08-07 13:42:41 -0700

    Update plugins.

--- wp-content/plugins/wp-google-maps/lib/codecabin/class.settings.php
+++ wp-content/plugins/wp-google-maps/lib/codecabin/class.settings.php
@@ -2,6 +2,9 @@
 
 namespace codecabin;
 
+if(!defined('ABSPATH'))
+	return;
+
 class Settings implements \ArrayAccess, \JsonSerializable, \IteratorAggregate
 {
 	private $data;
@@ -21,7 +24,10 @@ class Settings implements \ArrayAccess, \JsonSerializable, \IteratorAggregate
 		if(empty($option_value))
 			$this->data = (object)array();
 		else if(!($this->data = json_decode($option_value)))
-			throw new \Exception("Option value is not valid JSON");
+		{
+			global $wpdb;
+			throw new \Exception("Option value is not valid JSON in {$wpdb->prefix}options - " . json_last_error_msg());
+		}
 		
 		$this->overrides = (object)array();
 	}
